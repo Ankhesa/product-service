7e65aaf7cf6ce9736dd5a7621acf7a4c8af0eaf2
Johana Requena <jha.requena@gmail.com>
2021-05-20 19:27:53 -0300

    Inicializacion del repositorio product-service con configuraciones iniciales

---
+++ src/test/java/com/bigp/productserviceasp/ProductServiceAspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigp.productserviceasp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceAspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
