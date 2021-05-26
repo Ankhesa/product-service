72a5aed9147ddd8a45ad4d8735a1c6974d638035
Johana Requena <jha.requena@gmail.com>
2021-05-26 19:34:04 -0300

    Se crean clases de Category

--- src/main/java/com/bigp/productserviceasp/rest/controller/CategoryController.java
+++ src/main/java/com/bigp/productserviceasp/rest/controller/CategoryController.java
@@ -1,4 +1,23 @@
 package com.bigp.productserviceasp.rest.controller;
 
+import com.bigp.productserviceasp.rest.controller.domain.CategoryResponse;
+import com.bigp.productserviceasp.rest.controller.domain.ProductResponse;
+import com.bigp.productserviceasp.service.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 public class CategoryController {
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/categories")
+    @ResponseBody
+    public ResponseEntity<CategoryResponse> getCategories() {
+        return new ResponseEntity<>(categoryService.getCategories(), HttpStatus.OK);
+    }
 }
--- src/main/java/com/bigp/productserviceasp/service/CategoryService.java
+++ src/main/java/com/bigp/productserviceasp/service/CategoryService.java
@@ -2,6 +2,7 @@ package com.bigp.productserviceasp.service;
 
 import com.bigp.productserviceasp.rest.controller.domain.Category;
 import com.bigp.productserviceasp.rest.controller.domain.CategoryResponse;
+import com.google.common.collect.Lists;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,13 +10,20 @@ import java.util.List;
 @Service
 public class CategoryService {
 
-    private static final String FURNITURE_CATEGORY = "1";
+    /*private static final String FURNITURE_CATEGORY = "1";
     private static final String FOOD_CATEGORY = "2";
-    private static final String APPLIANCE_CATEGORY = "3";
+    private static final String APPLIANCE_CATEGORY = "3"; */
 
-    public CategoryResponse getCategories(){return new CategoryResponse(getCategoriesList())}
+    public CategoryResponse getCategories(){return new CategoryResponse(getCategoriesList());}
 
     private List<Category> getCategoriesList() {
+        Category cat1 = buildCategory("1", "1", "Furniture",1);
+        Category cat2 = buildCategory("2", "3", "Food",1);
+        Category cat3 = buildCategory("3", "2", "Appliances",0);
+        return Lists.newArrayList(cat1,cat2,cat3);
+    }
 
+    private Category buildCategory(String id, String rank, String name, int active) {
+        return new Category(id,rank,name,active);
     }
 }
